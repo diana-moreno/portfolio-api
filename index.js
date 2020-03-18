1e5c344bdb096b02698eba3cce86b37d3f6fc5d6
Diana <d7@hotmail.es>
2020-03-18 01:25:32 +0100

    change hotmail

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const jsonBodyParser = bodyParser.json()
 
 const contactAddress = process.env.GMAIL_ADDRESS
 const mailer = nodemailer.createTransport({
-  service: "gmail",
+  service: "hotmail",
   auth: {
     user: process.env.GMAIL_ADDRESS,
     pass: process.env.GMAIL_PASSWORD,
